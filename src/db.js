cc4a12163de03e76084defe6267a520da2ca62a5
MinhQuy <quy.vm@geekup.vn>
2022-04-12 17:33:49 +0700

    update

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 async function connectDB() {
-	const uri = "mongodb://mongodb:27017";
+	const uri = "mongodb://localhost:27017";
 
 	const client = new MongoClient(uri);
 
